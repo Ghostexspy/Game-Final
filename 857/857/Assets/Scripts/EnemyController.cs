15d7197241091f8adbb00af15fbee809baebe69b
Scala <733157@student.dusd.net>
2022-05-16 11:27:06 -0700

    Code
    Made scrip[t for guard movement as well as some tilemap tweaks

--- 857/857/Assets/Scripts/EnemyController.cs
+++ 857/857/Assets/Scripts/EnemyController.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float speed;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidBody2D;
+    Animator anim;
+    int direction = 1;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -15,4 +25,26 @@ public class EnemyController : MonoBehaviour
     {
         
     }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidBody2D.position;
+
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+            anim.SetFloat("MoveX", 0);
+            anim.SetFloat("MoveY", direction);
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+            anim.SetFloat("MoveX", direction);
+            anim.SetFloat("MoveY", 0);
+        }
+
+        rigidBody2D.MovePosition(position);
+    }
+
 }
+
